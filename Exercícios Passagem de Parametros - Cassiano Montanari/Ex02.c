f7c9d16b392f8d9e4be93a7eb48718f58d198ba9
Cassiano Montanari <cassianomontanari@gmail.com>
2017-09-18 21:43:25 -0300

    Lista de exercícios 6

--- Exercícios Passagem de Parametros - Cassiano Montanari/Ex02.c
+++ Exercícios Passagem de Parametros - Cassiano Montanari/Ex02.c
@@ -2,47 +2,85 @@
 #include<stdlib.h>
 #include<time.h>
 
-float valorA = 0;
-float valorB = 0;
+int valorA = 0;
+int valorB = 0;
 
 void soma(){
-    printf("%.2f + %.2f = %.2f", valorA, valorB, (valorA + valorB));
+    printf("\nSoma: %d + %d = %d", valorA, valorB, (valorA + valorB));
 }
 
-void subtracao(float* valorA, float* valorB){
-    printf("%.2f - %.2f = %.2f", *valorA, *valorB, ((*valorA) + (*valorB)));
+void subtracao(int* valorA, int* valorB){
+    printf("\nSubtracao: %d - %d = %d", *valorA, *valorB, ((*valorA) + (*valorB)));
 }
 
-float multiplicacao(){
-    float result = 0;
+int multiplicacao(){
+    int result = 0;
 
     result = valorA * valorB;
 
     return result;
 }
 
-float divisao(float* valorA, float* valorB){
-    float result = 0;
+int divisao(int* valorA, int* valorB){
+    int result = 0;
 
     result = ((*valorA) / (*valorB));
 
     return result;
 }
 
-int main(){
+int resto(int* valorA, int* valorB){
+    int result = 0;
 
-    printf("Digite o primeiro valor: ");
-    scanf("%f", &valorA);
+    result = ((*valorA) % (*valorB));
 
-    printf("Digite o segundo valor: ");
-    scanf("%f", &valorB);
-
-    //soma();
-    //subtracao(&valorA, &valorB);
-
-    //printf("%.2f x %.2f = %.2f", valorA, valorB, multiplicacao());
-
-    printf("%.2f / %.2f = %.2f", divisao(&valorA, &valorB));
-
+    return result;
 }
 
+int main(){
+
+    int escolha;
+
+    printf("Digite um valor de 1 a 5: ");
+    printf("\n1 - Soma");
+    printf("\n2 - Subtracao");
+    printf("\n3 - Multiplicacao");
+    printf("\n4 - Divisao");
+    printf("\n5 - Resto\n");
+    printf("Escolha:");
+    scanf("%d", &escolha);
+
+    printf("\n\nDigite o primeiro valor: ");
+    scanf("%d", &valorA);
+
+    printf("\nDigite o segundo valor: ");
+    scanf("%d", &valorB);
+
+    switch (escolha){
+        case 1 :
+            soma();
+        break;
+
+        case 2 :
+            subtracao(&valorA, &valorB);
+        break;
+
+        case 3 :
+            printf("%d x %d = %d", valorA, valorB, multiplicacao());
+        break;
+
+        case 4 :
+            printf("%d / %d = %d", valorA, valorB, divisao(&valorA, &valorB));
+        break;
+
+        case 5 :
+            printf("Resto de %d dividido por %d = %d", valorA, valorB, resto(&valorA, &valorB));
+        break;
+
+        default :
+            printf ("Valor invalido!\n");
+    }
+
+    getch();
+
+}
