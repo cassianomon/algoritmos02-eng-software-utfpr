4e772e9d7369391b3174772f9886edd9a635ff15
Cassiano Montanari <cassianomontanari@gmail.com>
2017-11-24 00:46:03 -0200

    1080 não finalizado - exceeded time limit

--- APS/urionlinejudge-1980.c
+++ APS/urionlinejudge-1980.c
@@ -2,34 +2,40 @@
 #include <string.h>
 #include <ctype.h>
 
-double fatorial(long a){
-    long result;
+typedef long long ll;
 
-    for (result = 1; a > 1; a--) {
-        result = result * a;
+ll fat[16];
+
+void fatorial(){
+    fat[0] = fat[1] = 1;
+    int i;
+
+    for (i = 2; i < 16; i++) {
+        fat[i] = i * fat[i - 1];
     }
+}
 
-    return result;
+ll obterFatorial(long n){
+    return fat[n];
 }
 
 int main(){
-
     long tamanho;
     long b;
-    char palavra[15];
-    int stop = 0;
+    char palavra[16];
 
-    while(stop == 0){
-        scanf(" %s", palavra);
+    fatorial();
 
-        if (isalpha(palavra) == "0"){
-            stop = 1;
+    while(1){
+        scanf(" %[^\n]s", palavra);
+
+        if (strcmp(palavra, "0") == 0){
             return 0;
         }
 
         tamanho = strlen(palavra);
 
-        b = fatorial(tamanho);
+        b = obterFatorial(tamanho);
         printf("%ld\n", b);
     }
 
